b706dd9bf182bc3ff1466df448a0709ff19e37e4
Easley <jessicaeasley@Jessicas-Air.attlocal.net>
2022-10-28 08:01:46 -0500

    added if statement for time interval to set end integer fo r countdown, added event listeners for countdown and startquiz functions, added a hide display for star quiz button when quiz is commenced

--- Assets/script.js
+++ Assets/script.js
@@ -1,14 +1,25 @@
 var timerEl = document.getElementById('time');
 var mainEl = document.getElementById('main');
-var buttonEl = document.getElementById('start quiz');
+var buttonEl = document.getElementById('startquiz');
 
 function countdown() {
     var timeLeft = 75;
     timerEl.innerHTML = timeLeft
     var timeInterval = setInterval(function () {
+        if (timeLeft <= 0) {
+            clearInterval(timeInterval)
+            timeLeft = 0
+        }
     timeLeft = timeLeft-1
     timerEl.innerHTML = timeLeft
     }, 1000);
   }
 
+  function startQuiz(){
+    buttonEl.classList.add("hide")
+
+  }
+
+
   buttonEl.addEventListener("click",countdown);
+  buttonEl.addEventListener("click",startQuiz);
