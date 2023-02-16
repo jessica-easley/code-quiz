c1380b51ce0164e0f7bd3d64eb45b4934f9ee1d7
Easley <jessicaeasley@Jessicas-Air.attlocal.net>
2023-02-16 10:03:07 -0600

    updated button styling

--- Assets/script.js
+++ Assets/script.js
@@ -1,68 +1,68 @@
-var timerEl = document.getElementById('time');
-var mainEl = document.getElementById('main');
-var startButtonEl = document.getElementById('startquiz');
-var endButtonEl = document.getElementById('submit-quiz');
-var questionsEl = document.getElementById('questions');
-var answerChoicesEl = document.getElementById('choices');
-var initialsEl = document.querySelector('initials');
-var timeInterval
-var timeLeft
+var timerEl = document.getElementById("timer");
+var mainEl = document.getElementById("main");
+var startButtonEl = document.getElementById("startquiz");
+var endButtonEl = document.getElementById("submitquiz");
+var questionsEl = document.getElementById("questions");
+var answerChoicesEl = document.getElementById("choices");
+var initialsEl = document.querySelector("initials");
+var timeInterval;
 var score = 0;
 
 // Timer Counterdown & Start Quiz
 function countdown() {
-    var timeLeft = 75;
-    timerEl.innerHTML = timeLeft
-    timeInterval = setInterval(function () {
-        if (timeLeft <= 0) {
-            clearInterval(timeInterval)
-            timeLeft = 0
-        }
-    timerEl.innerHTML = timeLeft
-    timeLeft = timeLeft-1
-    }, 1000);
-  }
-
-  function startQuiz(){
-    buttonEl.classList.add("hide")
-
-  }
-
-
-  buttonEl.addEventListener("click",countdown);
-  buttonEl.addEventListener("click",startQuiz);
-
-  // Quiz Questions & Answer Choices
-
-  var questions = [
-    {
-      question: "Commonly Used Data Types DO NOT Include:",
-      choices: ["strings", "alerts", "booleans", "numbers"],
-      answer: "alerts"
-
-    },
-    {
-      question: "The Condition in an if/else statement is enclosed with ____.",
-      choices: ["quotes", "curly brackets", "parenthesis", "square brackets"],
-      answer: "parenthesis"
-
-    },
-    {
-      question: "Arrays in JavaScript can be used to store ___.",
-      choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
-      answer: "all of the above"
-
-    },
-    {
-      question: "String Values must be enclosed within ____ when being assigned to variables.",
-      choices: ["commas", "curly brackets", "quotes", "parenthesis"],
-      answer: "quotes"
-
-    },
-    {
-      question: "A very useful tool used during development and debugging for printing content to the debugger is:",
-      choices: ["JavaScript", "terminal/bash", "for loops", "console log"],
-      answer: "console log"
-
+  var timeLeft = 75;
+  timerEl.innerHTML = timeLeft;
+  timeInterval = setInterval(function () {
+    if (timeLeft <= 0) {
+      clearInterval(timeInterval);
+      timeLeft = 0;
     }
-  ]
+    timerEl.innerHTML = timeLeft;
+    timeLeft = timeLeft - 1;
+  }, 1000);
+}
+
+function startQuiz() {
+  buttonEl.classList.add("hide");
+}
+
+buttonEl.addEventListener("click", countdown);
+buttonEl.addEventListener("click", startQuiz);
+
+// Quiz Questions & Answer Choices
+
+var questions = [
+  {
+    question: "Commonly ssed data types DO NOT include:",
+    choices: ["A. strings", "B. alerts", "C. booleans", "D. numbers"],
+    answer: "B. alerts",
+  },
+  {
+    question: "The Condition in an if/else statement is enclosed with ____.",
+    choices: ["A. quotes", "B. curly brackets", "C. parenthesis", "D. square brackets"],
+    answer: "C. parenthesis",
+  },
+  {
+    question: "Arrays in JavaScript can be used to store ___.",
+    choices: ["A. numbers and strings", "B. other arrays","C. booleans","D. all of the above"],
+    answer: "D. all of the above",
+  },
+  {
+    question:
+      "String Values must be enclosed within ____ when being assigned to variables.",
+    choices: ["A. commas", "B. curly brackets", "C. quotes", "D. parenthesis"],
+    answer: "C. quotes",
+  },
+  {
+    question:
+      "A very useful tool used during development and debugging for printing content to the debugger is:",
+    choices: ["A. JavaScript", "B. terminal/bash", "C. for loops", "D. console log"],
+    answer: "D. console log",
+  },
+];
+
+//  Additional Variables
+// var secondsLeft = 75;
+// var questionNumber = 0;
+// var totalScore = 0;
+// var questionCount = 1;
